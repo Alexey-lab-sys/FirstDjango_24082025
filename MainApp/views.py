741a8a4817149023925888e92dc7835fd12fe873
Alexey Trebunskiy <a.trebounskiy@gmail.com>
2025-08-28 21:37:48 +0300

    Done. m1:task2

--- MainApp/views.py
+++ MainApp/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import get_object_or_404
 
 # Create your views here.
+
+USER_DATA = {
+    'first_name': 'Иван',
+    'middle_name': 'Петрович',
+    'last_name': 'Иванов',
+    'phone': '8-923-600-01-02',
+    'email': 'vasya@mail.ru'
+    }
+
 
 
 def home(request):
@@ -11,3 +21,20 @@ def home(request):
     """
 
     return HttpResponse(text)
+
+
+
+def about(request):
+    return HttpResponse(f"""
+    Имя: {USER_DATA['first_name']}<br>
+    Отчество: {USER_DATA['middle_name']}<br>
+    Фамилия: {USER_DATA['last_name']}<br>
+    телефон: {USER_DATA['phone']}<br>
+    email: {USER_DATA['email']}<br><br>
+    <a href="/">На главную</a>
+    """)
+ 
+
+
+
+
