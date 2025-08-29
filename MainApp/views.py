b82b102e9c3fd78faeb05cec921b31a19928faa2
Alexey Trebunskiy <a.trebounskiy@gmail.com>
2025-08-29 21:16:26 +0300

    Correction.M1:task3

--- MainApp/views.py
+++ MainApp/views.py
@@ -1,19 +1,24 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+#from django.shortcuts import render
+#from django.http import HttpResponse
+#from django.http import HttpResponse, Http404
+#from django.urls import reverse
+
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, Http404
 from django.urls import reverse
 
 # Create your views here.
 
+# Данные пользователя
 USER_DATA = {
     'first_name': 'Иван',
     'middle_name': 'Петрович',
     'last_name': 'Иванов',
     'phone': '8-923-600-01-02',
     'email': 'vasya@mail.ru'
-    }
+}
 
-
+# Список товаров
 items = [
     {"id": 1, "name": "Кроссовки abibas"},
     {"id": 2, "name": "Куртка кожаная"},
@@ -22,38 +27,52 @@ items = [
     {"id": 5, "name": "Кепка"},
 ]
 
-
-
 def home(request):
-    text = """
+    html = f'''
     <h1>"Изучаем django"</h1>
-    <strong>Автор</strong>: <i>Иванов И. П.</i>
-    """
-
-    return HttpResponse(text)
-
-
+    <strong>Автор</strong>: <i>{USER_DATA["last_name"]} {USER_DATA["first_name"][0]}.{USER_DATA["middle_name"][0]}.</i>
+    <br><br>
+    <a href="{reverse('about')}">Обо мне</a> |
+    <a href="{reverse('items_list')}">Список товаров</a>
+    '''
+    return HttpResponse(html)
 
 def about(request):
-    return HttpResponse(f"""
+    html = f"""
     Имя: {USER_DATA['first_name']}<br>
     Отчество: {USER_DATA['middle_name']}<br>
     Фамилия: {USER_DATA['last_name']}<br>
     телефон: {USER_DATA['phone']}<br>
     email: {USER_DATA['email']}<br><br>
-    <a href="/">На главную</a>
-    """)
- 
-
-
-
+    <a href="{reverse('home')}">На главную</a> |
+    <a href="{reverse('items_list')}">Список товаров</a>
+    """
+    return HttpResponse(html)
 
 def item_detail(request, item_id):
-    # Находим товар по id
-    item = next((item for item in items if item["id"] == item_id), None)
+    """Страница товара по ID"""
+    # Ищем товар по ID, если не найден, вызываем 404
+    item = next((item for item in items if item['id'] == item_id), None)
     
-    if item is not None:
-        return HttpResponse(item["name"])
+    if item:
+        html = f"""
+        <h2>{item['name']}</h2>
+        ID товара: {item['id']}<br><br>
+        <a href="{reverse('items_list')}">← Назад к списку товаров</a><br>
+        <a href="{reverse('home')}">На главную</a> | 
+        <a href="{reverse('about')}">Обо мне</a>
+        """
     else:
-        return HttpResponse("Товар не найден", status=404)
+        return HttpResponse("<h2>Товар не найден</h2><br><a href='{reverse('items_list')}'>Назад к списку товаров</a>")
+    
+    return HttpResponse(html)
 
+def items_list(request):
+    """Страница со списком товаров"""
+    html = "<h2>Список товаров</h2><ul>"
+    for item in items:
+        html += f'<li><a href="{reverse("item_detail", args=[item["id"]])}">{item["name"]}</a></li>'
+    html += "</ul>"
+    html += f'<a href="{reverse("home")}">На главную</a> | '
+    html += f'<a href="{reverse("about")}">Обо мне</a>'
+    return HttpResponse(html)
