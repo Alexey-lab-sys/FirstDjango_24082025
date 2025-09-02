72eca639740eb5a1799424551eece44820fdddcb
Alexey Trebunskiy <a.trebounskiy@gmail.com>
2025-09-02 18:18:15 +0300

    Done. M3.2:task1-3

--- MainApp/models.py
+++ MainApp/models.py
@@ -6,5 +6,7 @@ class Item(models.Model):
    name  = models.CharField(max_length=100)
    brand = models.CharField(max_length=100)
    count = models.PositiveIntegerField()
+   description = models.TextField()
+   description = models.CharField(max_length=255, default='Нет описания')
 
 
